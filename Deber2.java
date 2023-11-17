6dd8a6d103bb329c35ce422c27bd2b65c14a996b
JohnMata0427 <john.mata@epn.edu.ec>
2023-11-16 23:06:56 -0500

    Corrección de tabulación de la antepenúltima llave y añadí mi nombre

--- Deber2.java
+++ Deber2.java
@@ -1,7 +1,7 @@
 public class Deber2 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola, mi nombre es John Mata");
         System.out.println("Hola, soy Kevin Coloma");
         System.out.println("Hola, me llamo Isabel Pazto");
-           }
+    }
 }
