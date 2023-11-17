b718a168494398298d3ba34d204f2cefb4490267
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2023-11-16 20:34:39 -0500

    Ingrese mi nombre

--- Deber2.java
+++ Deber2.java
@@ -2,5 +2,6 @@ public class Deber2 {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
         System.out.println("Hola, soy Kevin Coloma");
+        System.out.println("Hola, me llamo Isabel Pazto");
            }
 }
